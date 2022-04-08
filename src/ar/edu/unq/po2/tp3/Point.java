7c99de9186b413decbceb98d74b401f9ae99df7b
athan <athan@DESKTOP-IFTC17F>
2022-04-08 14:07:35 -0300

    Agregado el ejercicio Point

---
+++ src/ar/edu/unq/po2/tp3/Point.java
@@ -0,0 +1,46 @@
+package ar.edu.unq.po2.tp3;
+
+public class Point {
+
+	int x;
+	int y;
+	
+	public Point() {
+		setX(0);
+		setY(0);
+	}
+
+	public Point(int x, int y) {
+		setX(x);
+		setY(y);
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public void moverA(int x, int y) {
+		setX(x);
+		setY(y);
+	}
+	
+	
+	public Point sumarPuntos(Point unPunto) {
+		setX(this.getX() + unPunto.getX());
+		setY(this.getY() + unPunto.getY());
+		return unPunto;
+	}
+	
+}
