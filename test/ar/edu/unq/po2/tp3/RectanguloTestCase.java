0fc7646eba0dfb2d3ef3d97fd37cac84c7bb1bf2
athan <athan@DESKTOP-IFTC17F>
2022-04-08 16:01:48 -0300

    Testing del ejercicio 9

---
+++ test/ar/edu/unq/po2/tp3/RectanguloTestCase.java
@@ -0,0 +1,59 @@
+package ar.edu.unq.po2.tp3;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RectanguloTestCase {
+
+	Point point;
+	Rectangulo rectangulo;
+	Cuadrado cuadrado;
+	
+	@BeforeEach
+	public void setUp() {
+		point = new Point(0,0);
+		rectangulo = new Rectangulo(point, 6, 4);
+		cuadrado = new Cuadrado(point, 4);
+	}
+	
+	
+	@Test
+	void calcularArea() {
+		assertEquals(24, rectangulo.calcularArea());
+	}
+	
+	@Test
+	void calcularPerimetro() {
+		assertEquals(20, rectangulo.calcularPerimetro());
+	}
+	
+	@Test
+	void esVertical() {
+		assertFalse(rectangulo.esVertical());
+	}
+
+	@Test
+	void esHorizontal() {
+		assertTrue(rectangulo.esHorizontal());
+	}
+	
+	
+	@Test
+	void alturaYBaseCuadrado() {
+		assertEquals(4, cuadrado.getAltura());
+		assertEquals(4, cuadrado.getBase());
+	}
+	
+	@Test
+	void areaCuadrado() {
+		assertEquals(16, cuadrado.calcularArea());
+	}
+	
+	@Test
+	void perimetroCuadrado() {
+		assertEquals(16, cuadrado.calcularPerimetro());
+	}
+	
+}
