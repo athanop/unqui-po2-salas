f54dcb25fc25e862c1b80562407e1f1d3a96a7ec
athanop <elnahue.-@live.com.ar>
2022-08-31 19:24:31 -0300

    Counter terminado

---
+++ src/ar/edu/unq/po2/práctica_3/Counter.java
@@ -0,0 +1,90 @@
+package ar.edu.unq.po2.práctica_3;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Counter {
+
+
+	List<Integer> num = new ArrayList<Integer>();
+	
+	
+	//---------------------------------------------------------EJERCICIO 1-------------------------------------------------------------------------
+	public Integer pares() {
+		
+		List<Integer> pares = this.num.stream().filter(n -> n%2==0).collect(Collectors.toList());
+		return pares.size();
+	}
+
+
+	public void addNumber(int i) {
+		this.num.add(i);
+	}
+
+
+	public Integer impares() {
+		List<Integer> impares = this.num.stream().filter(n -> n%2==1).collect(Collectors.toList());
+		return impares.size();
+	}
+	
+	public Integer esMultiplo(int i) {
+		List<Integer> multiplos = this.num.stream().filter(n -> n%i==0).collect(Collectors.toList());
+		return multiplos.size();
+	}
+	
+	//---------------------------------------------------------EJERCICIO 2-------------------------------------------------------------------------
+	
+		
+	public int desarmarNumeros(ArrayList<Integer> enteros) {
+		
+		int numerosConMasPares = enteros.get(0);
+		for(int entero:enteros) {
+			numerosConMasPares = numerosConMasParesEntre(numerosConMasPares, entero);
+		}		
+		return numerosConMasPares;
+	}
+
+	public int numerosConMasParesEntre(int x, int y) {
+		int numMayor = (numerosParesEn(x) >= numerosParesEn(y)) ? x : y;		
+		return numMayor;
+	}
+
+	//obtengo los numeros pares del digito
+	public int numerosParesEn(int x) {
+		for (int i = 0; i < cantidadDeDigitosDe(x); i++) {
+			this.addNumber(x % 10);
+			x = x / 10;
+		}
+		return (this.pares());
+	}
+	
+	public int cantidadDeDigitosDe(int numero) {
+		int cifras = 1;
+		int n = numero;
+		while(n >= 10) {
+			n = n/10;
+			cifras++;
+		}
+		return cifras;
+	}
+	
+	//---------------------------------------------------------EJERCICIO 3-------------------------------------------------------------------------
+	
+	
+	public int multiploEntre(int x, int y) {
+		int resultado = -1;
+		for(int i=0; i < 1000; i++) {
+			if(sonMultiplos(x,y,i)) {
+				resultado = i;
+			}
+		}
+		return resultado;
+	}
+
+	public boolean sonMultiplos(int x, int y, int i) {
+		return (i%x==0 && i%y==0);
+	}
+	
+	
+}
---
+++ test/ar/edu/unq/po2/práctica_3/CounterTestCase.java
@@ -0,0 +1,102 @@
+package ar.edu.unq.po2.práctica_3;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CounterTestCase {
+	
+		Counter counter;
+		ArrayList<Integer> arrayTest = new ArrayList<Integer>();
+		/**
+		* Crea un escenario de test básico, que consiste en un contador
+		* con 10 enteros
+		*
+		* @throws Exception
+		*/
+		
+		
+		@BeforeEach
+		public void setUp() throws Exception {
+		//Se crea el contador
+		counter = new Counter();
+		//Se agregan los numeros. Un solo par y nueve impares
+		counter.addNumber(1);
+		counter.addNumber(3);
+		counter.addNumber(5);
+		counter.addNumber(7);
+		counter.addNumber(9);
+		counter.addNumber(1);
+		counter.addNumber(1);
+		counter.addNumber(1);
+		counter.addNumber(1);
+		counter.addNumber(4);
+		
+		arrayTest.add(1111); arrayTest.add(2222); arrayTest.add(24688); arrayTest.add(2468888); 
+		}
+		
+		
+		@Test
+		public void testLaCantidadDeParesEsUno() {
+			int amount = counter.pares();
+			assertEquals(amount, 1);
+		}
+		
+		@Test
+		public void testLaCantidadDeImparesEsNueve() {
+			int amount = counter.impares();
+			assertEquals(amount, 9);
+		}
+		
+		@Test
+		public void testLaCantidadDeMultiplos() {
+			int amount = counter.esMultiplo(2);
+			assertEquals(amount, 1);
+		}
+		
+		//Test del ejercicio 2 Desarmando numeros
+
+		@Test
+		public void contadorDeDigitos() {
+			assertEquals(6, counter.cantidadDeDigitosDe(123456));
+		}
+
+
+		@Test
+		public void mayorCantidadDeParesEntreDosNumeros() {
+			assertEquals(2234,counter.numerosConMasParesEntre(1112, 2234));
+		}
+
+		@Test
+		public void digitoSinPares() {
+			Counter desarmar = new Counter();
+			int sinPares = desarmar.numerosParesEn(1111);
+			assertEquals(0,sinPares);
+		}
+
+
+		@Test
+		public void numeroConMasParesEnElArray() {
+			assertEquals(2468888, counter.desarmarNumeros(arrayTest));
+		}
+
+
+		//Test del ejercicio 3 Múltiplos
+
+		@Test
+		public void multiplosEntreDosNumeros() {
+			assertEquals(999, counter.multiploEntre(3, 9));
+		}
+
+
+		@Test
+		public void sonMultiplos() {
+			assertTrue(counter.sonMultiplos(3, 9,999));
+		}
+
+		
+		
+}
