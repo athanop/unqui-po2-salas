296ee1ac54a9f276cfce95b9650667e06a1be858
athanop <elnahue.-@live.com.ar>
2022-09-26 11:43:32 -0300

    Agrego ejercicio email

---
+++ src/ar/edu/unq/po2/práctica_6/ClienteEMail/IServidor.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.po2.práctica_6.ClienteEMail;
+
+import java.util.List;
+
+public interface IServidor {
+	
+	public List<Correo> recibirNuevos(String user, String pass);
+
+	public void conectar(String nombreUsuario, String passusuario);
+
+	public void enviar(Correo correo);
+	
+}
---
+++ src/ar/edu/unq/po2/práctica_6/ClienteEMail/IServidorPop.java
@@ -0,0 +1,11 @@
+package ar.edu.unq.po2.práctica_6.ClienteEMail;
+
+public interface IServidorPop {
+
+	public float tazaDeTransferencia();
+
+	public void resetear();
+	
+	public void realizarBackUp();
+
+}
