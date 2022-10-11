18bf38e82329c773efacec6a0a10f6a1ecb6f43f
athanop <elnahue.-@live.com.ar>
2022-10-10 23:25:06 -0300

    .

--- test/ar/edu/unq/po2/práctica_8_Observer/EncuentrosDeportivos/AplicacionMovilTestCase.java
+++ test/ar/edu/unq/po2/práctica_8_Observer/EncuentrosDeportivos/AplicacionMovilTestCase.java
@@ -23,12 +23,6 @@ class AplicacionMovilTestCase {
 		app = new AplicacionMovil(adm);
 		
 	}
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
-
 	
 	@Test
 	void testUnaAppSeSuscribeAlDeporteBasquet() {
